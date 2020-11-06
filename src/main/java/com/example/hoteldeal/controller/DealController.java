7359f0ac63bc904c29fb79301d0b4848d04fd25a
Madi <madizhaksigeldi@gmail.com>
2020-11-06 23:23:32 +0600

    Hystrix + Bulkhead

--- src/main/java/com/example/hoteldeal/controller/DealController.java
+++ src/main/java/com/example/hoteldeal/controller/DealController.java
@@ -3,11 +3,15 @@ package com.example.hoteldeal.controller;
 import com.example.hoteldeal.model.Deal;
 import com.example.hoteldeal.service.DealService;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,8 +25,20 @@ public class DealController {
         return service.getAllDeals();
     }
 
+    @HystrixCommand(fallbackMethod = "getFallbackDeal",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10"),
+            }
+    )
     @GetMapping(path = "/deals/{id}")
     public Optional<Deal> getDealsById(@PathVariable("id") long id) {
         return service.getDealsById(id);
+    }
+
+    public List<Deal> getFallbackDeal(@PathVariable("id") long id) {
+        List<Deal> dealList = new ArrayList<>();
+        dealList.add(new Deal());
+        return dealList;
     }
 }
